67a95e23048f0375c5ebe5bd361696bac10af25c
davronkhandamov <xamdamovdavron6@gmail.com>
2024-01-14 16:43:10 +0500

    added only js code to run

--- js/app.js
+++ js/app.js
@@ -5,4 +5,29 @@ editor.setOptions({
     enableBasicAutocompletion: true,
     enableSnippets: true,
     enableLiveAutocompletion: true,
+    wrap: 80,
+});
+editor.setValue(`function sayHello(){
+    return "Hello"
+}`);
+
+document.getElementById("editor").style.fontSize = "16px";
+const buttonEl = document.getElementById("button");
+const outputTextEl = document.querySelector(".outputtext");
+const selectEl = document.querySelector("#lang");
+
+buttonEl.addEventListener("click", () => {
+    const code = editor.getValue();
+    const output = eval(code);
+    outputTextEl.textContent = output;
+    sayHello();
+});
+selectEl.addEventListener("change", (e) => {
+    editor.setValue("");
+    editor.session.setMode("ace/mode/" + e.target.value);
+});
+window.addEventListener("keydown", (event) => {
+    if (event.ctrlKey && event.key === "Enter") {
+        buttonEl.click();
+    }
 });
